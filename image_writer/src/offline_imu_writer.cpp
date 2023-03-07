dc495c9ed1701d38a009ec6afb0291732e51081f
larrydong <jfjxxdy2007@126.com>
2023-03-07 23:06:11 +0800

    Add image&imu extractor

--- image_writer/src/offline_imu_writer.cpp
+++ image_writer/src/offline_imu_writer.cpp
@@ -1,14 +1,15 @@
 
 #include <iostream>
 #include <fstream>
-// #include <boost/foreach.hpp>
 
 #include <ros/ros.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Imu.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
+#include <geometry_msgs/Vector3.h>
 
 #include <opencv2/opencv.hpp>
 
@@ -19,15 +20,19 @@ using namespace cv;
 
 int main(int argc, char **argv){
 
-    ros::init(argc, argv, "offline_writer");
+    ros::init(argc, argv, "offline_imu_writer");
 	ros::NodeHandle nh;
+    
+    ROS_WARN("IMU");
 
     string input_bag("test.bag");
     string output_folder("/home/larrydong/imu_ws/output");
     string image_topic("/image");
+    string imu_topic("/imu/data");
 
     ros::param::get("~input_bag", input_bag);               //
     ros::param::get("~image_topic", image_topic);               //
+    ros::param::get("~imu_topic", imu_topic);               //
     ros::param::get("~output_folder", output_folder);       // get videoX, X is defined in roslaunch file.
     
     rosbag::Bag bag;
@@ -36,12 +41,15 @@ int main(int argc, char **argv){
         ROS_ERROR_STREAM("Cannot open bag: " << input_bag);
     }
 
-    vector<double> timestamps;
+    vector<double> image_timestamps, imu_timestamps;
     vector<Mat> images;
+    vector<geometry_msgs::Vector3> accs, gyros;
 
     std::vector<std::string> topics;
     topics.push_back(image_topic);
-    ROS_INFO_STREAM("Begin to extract: " << image_topic << " from: " << input_bag);
+    topics.push_back(imu_topic);
+    ROS_INFO_STREAM("Begin to extract image topic: " << image_topic);
+    ROS_INFO_STREAM("Begin to extract imu topic: " << imu_topic);
 
     rosbag::View view(bag, rosbag::TopicQuery(topics));
     for (rosbag::MessageInstance const m : view){
@@ -60,29 +68,55 @@ int main(int argc, char **argv){
             Mat img = cv_ptr->image;
             long ts = cv_ptr->header.stamp.toNSec();
             images.push_back(img);
-            timestamps.push_back(ts);
+            image_timestamps.push_back(ts);
+        }
+        else if(m.getTopic() == imu_topic){
+            sensor_msgs::Imu::ConstPtr msg = m.instantiate<sensor_msgs::Imu>();
+            auto gyro = msg->angular_velocity;
+            auto acc = msg->linear_acceleration;
+            accs.push_back(acc);
+            gyros.push_back(gyro);
+            imu_timestamps.push_back(msg->header.stamp.toNSec());
+            // cout << "gyro: " << gyro.x << ", " << gyro.y << ", " << gyro.z << endl;
+            // cout << "acc : " << acc.x << ", " << acc.y << ", " << acc.z << endl;
         }
         else{
             cerr << "unpected topics" << endl;
+            std::abort();
         }
     }
 
-    string ts_filename = output_folder + "/ts.csv";
+    string image_ts_filename = output_folder + "/ts.csv";
     string image_folder = output_folder + "/image";
-    ROS_INFO_STREAM("Saving images and ts to: " << image_folder);
-    ofstream of(ts_filename);
+    string imu_filename = output_folder + "/imu.csv";
+    ROS_INFO_STREAM("Saving images, image_ts, and imu...");
+    
+    // save image and image_ts
+    ofstream of(image_ts_filename);
     for (int i = 0; i < images.size(); ++i){
         Mat img = images[i];
-        long ts = timestamps[i];
+        long ts = image_timestamps[i];
         string image_filename = image_folder + "/" + to_string(ts) + ".bmp";
         imwrite(image_filename, img);
         of << ts << endl;
     }
     of.close();
-    ROS_INFO_STREAM("Saved images number: " << images.size());
-    ROS_INFO_STREAM("Saved ts to: " << ts_filename);
+    ROS_INFO_STREAM("Saved " << images.size() << " images to: " << image_folder);
+    ROS_INFO_STREAM("Saved " << image_timestamps.size() << " image_ts to: " << image_ts_filename);
+
+    // save IMU.
+    of.open(imu_filename);
+    of << "# ts [ns],wx [rad s^-1],wy [rad s^-1],wz [rad s^-1],ax [m s^-2],ay [m s^-2],az [m s^-2]" << endl;
+    for (int i = 0; i < accs.size(); ++i){
+        auto acc = accs[i];
+        auto gyro = gyros[i];
+        long ts = imu_timestamps[i];
+        of << ts << "," << gyro.x << "," << gyro.y << "," << gyro.z << "," << acc.x << "," << acc.y << "," << acc.z << endl;
+    }
+    of.close();
+    ROS_INFO_STREAM("Saved "<< imu_timestamps.size()<< " imu to: " << imu_filename);
+
     ROS_WARN("Done without error.");
-    
     return 0;
 }
 
